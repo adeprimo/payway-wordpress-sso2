c247f043e1caa8349df1eca4a91b613fc62534e4
Magnus Rosenquist Hamvall <magnus.rosenquist@adeprimo.se>
2024-10-16 13:54:00 +0200

    Bump version

--- wp-tulo-payway.php
+++ wp-tulo-payway.php
@@ -9,13 +9,13 @@
  * that starts the plugin.
  *
  * @link              https://adeprimo.se
- * @since             1.2.5.0-cookie-beta2
+ * @since             1.2.5.1-cookie
  * @package           Tulo_Payway_Server
  *
  * @wordpress-plugin
  * Plugin Name:       Tulo Payway Connector for Wordpress
  * Description:       This plugin integrates with the SSO2 single sign on solution in Tulo Payway. Now with support for Tulo Paywall.
- * Version:           1.2.5.0-cookie-beta2
+ * Version:           1.2.5.1-cookie
  * Author:            Adeprimo AB
  * Author URI:        https://adeprimo.se
  * Text Domain:       tulo
